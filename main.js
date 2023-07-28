a1d22b8476e2d93c6c90ab2533b714473cf4d7f6
Ann Chou <yuanchou.ann@gmail.com>
2023-07-28 10:16:35 +0800

    modify picture path

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const storeList = [
       hours: "營業時間：10:00-21:00",
       address: "地址：台北市中山區南京東路25巷2-1號",
       photo:
-        "assets/image/store-1.png",
+        "../assets/image/store-1.png",
       map:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.4636138459286!2d121.53300454855906!3d25.0522709805177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abdf92494d29%3A0xd908a8a881f73476!2zMTA0OTHlj7DljJfluILkuK3lsbHljYDljZfkuqzmnbHot6_kuInmrrUyNeiZnw!5e0!3m2!1szh-TW!2stw!4v1689689324255!5m2!1szh-TW!2stw"
     },
@@ -19,7 +19,7 @@ const storeList = [
       hours: "營業時間：10:00-21:00",
       address: "地址：台北市中正區復興南路 132-1 號",
       photo:
-        "assets/image/store-2.png",
+        "../assets/image/store-2.png",
       map:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.615088930526!2d121.54113651123167!3d25.047133437669444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abdbf61946df%3A0x3c99fa1b2467da21!2z5b6p6IiI5Y2X6Lev!5e0!3m2!1szh-TW!2stw!4v1689738423666!5m2!1szh-TW!2stw"
     },
@@ -29,7 +29,7 @@ const storeList = [
       hours: "營業時間：10:00-21:00",
       address: "地址：台中市清水區經南一路 23 號 8 樓",
       photo:
-        "assets/image/store-3.png",
+        "../assets/image/store-3.png",
       map:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2078.585536393451!2d120.70044460975465!3d24.21803310729417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346917660c1fff99%3A0xd921b3953d98ae27!2zNDI35Y-w5Lit5biC5r2t5a2Q5Y2A5Y2X5LiA6Lev!5e0!3m2!1szh-TW!2stw!4v1689738558821!5m2!1szh-TW!2stw"
     },
@@ -39,7 +39,7 @@ const storeList = [
       hours: "營業時間：10:00-21:00",
       address: "地址：高雄市苓雅區中正路 38 號 12 樓",
       photo:
-        "assets/image/store-4.png",
+        "../assets/image/store-4.png",
       map:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.7639136856073!2d120.338210411173!3d22.62528963095086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e04b492b36c93%3A0xde28107e320c1921!2zODAy6auY6ZuE5biC6IuT6ZuF5Y2A5Lit5q2j5LiA6LevMzjomZ8!5e0!3m2!1szh-TW!2stw!4v1689751386260!5m2!1szh-TW!2stw"
     },
@@ -49,7 +49,7 @@ const storeList = [
       hours: "營業時間：10:00-21:00",
       address: "地址：高雄市前鎮區中華一路 63 號 6 樓",
       photo:
-        "assets/image/store-5.png",
+        "../assets/image/store-5.png",
       map:
         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.641781355878!2d120.287666411174!3d22.667140829412258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e05aebd7519bb%3A0x53dcd3769d59162d!2zODA06auY6ZuE5biC6byT5bGx5Y2A5Lit6I-v5LiA6LevNjPomZ8!5e0!3m2!1szh-TW!2stw!4v1689738723761!5m2!1szh-TW!2stw"
     }
@@ -87,7 +87,7 @@ const storeList = [
   function getDefaultLocationData(){
     return `<div id="locationContent" class="location-content d-flex f-wrap gap-24">
     <div class="location-card">
-        <img class="location-photo" src="assets/image/store-1.png" alt="台北中山旗艦店">
+        <img class="location-photo" src="../assets/image/store-1.png" alt="台北中山旗艦店">
         <p class="location-title fz-24">台北中山旗艦店</p>
         <ul class="location-detail fz-20">
             <li class="mb-8 d-flex f-aib"><span class="material-icons-outlined">
@@ -110,7 +110,7 @@ const storeList = [
             type="button">詳細資訊</a>
     </div>
     <div class="location-card">
-        <img class="location-photo" src="assets/image/store-2.png" alt="台北綠園店">
+        <img class="location-photo" src="../assets/image/store-2.png" alt="台北綠園店">
         <p class="location-title fz-24">台北綠園店</p>
         <ul class="location-detail fz-20">
             <li class="mb-8 d-flex f-aib"><span class="material-icons-outlined">
@@ -132,7 +132,7 @@ const storeList = [
         <a href="#" class="btn-store ta-center d-block bg-black fc-white" type="button">詳細資訊</a>
     </div>
     <div class="location-card">
-        <img class="location-photo" src="assets/image/store-3.png" alt="台中清水旗艦店">
+        <img class="location-photo" src="../assets/image/store-3.png" alt="台中清水旗艦店">
         <p class="location-title fz-24">台中清水旗艦店</p>
         <ul class="location-detail fz-20">
             <li class="mb-8 d-flex f-aib"><span class="material-icons-outlined">
@@ -154,7 +154,7 @@ const storeList = [
         <a href="#" class="btn-store ta-center d-block bg-black fc-white" type="button">詳細資訊</a>
     </div>
     <div class="location-card">
-        <img class="location-photo" src="assets/image/store-4.png" alt="高雄中正形象店">
+        <img class="location-photo" src="../assets/image/store-4.png" alt="高雄中正形象店">
         <p class="location-title fz-24">高雄中正形象店</p>
         <ul class="location-detail fz-20">
             <li class="mb-8 d-flex f-aib"><span class="material-icons-outlined">
@@ -176,7 +176,7 @@ const storeList = [
         <a href="#" class="btn-store ta-center d-block bg-black fc-white" type="button">詳細資訊</a>
     </div>
     <div class="location-card">
-        <img class="location-photo" src="assets/image/store-5.png" alt="高雄夢時代店">
+        <img class="location-photo" src="../assets/image/store-5.png" alt="高雄夢時代店">
         <p class="location-title fz-24">高雄夢時代店</p>
         <ul class="location-detail fz-20">
             <li class="mb-8 d-flex f-aib"><span class="material-icons-outlined">
